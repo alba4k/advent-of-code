1d1615a2e1ff517ecfb0f3eecd2ea501ef90f151
alba4k <blaskoazzolaaaron@gmail.com>
2025-12-02 11:07:19 +0100

    2025: day2

---
+++ 2025/day02/part1.py
@@ -0,0 +1,25 @@
+#!/bin/python
+
+def repeatsPattern(number: str) -> bool:
+    m: int = len(number)//2
+    return (number[:m] == number[m:])
+    
+sum: int = 0
+
+with open("input", "r") as file:
+    input = file.read()
+
+ranges: list[str] = input.split(",")
+
+for id_range_str in ranges:
+    id_range: list[str] = id_range_str.split("-")
+    print(id_range, id_range_str)
+
+    for i in range(int(id_range[0]), int(id_range[1])+1):
+        if repeatsPattern(str(i)):
+            m: int = len(str(i))//2
+            print(i, "repeats", str(i)[:m])
+            sum += i
+
+print(sum)
+
---
+++ 2025/day02/part2.py
@@ -0,0 +1,38 @@
+#!/bin/python
+from math import floor, sqrt
+
+def divisors(number: str) -> list[int]:
+    result: list[int] = []
+    for i in range(1, number):
+        if number/i == float(number//i):
+            result.append(i)
+    return result
+
+def repeatsPattern(number: str) -> bool:
+    for length in divisors(len(number)):
+        for i in range(1, len(number)//length):
+            allEqual: bool = True
+            if(number[i*length:(i+1)*length] != number[:length]):
+                allEqual = False
+                break
+                
+        if(allEqual):
+            return True
+    return False
+    
+    
+sum: int = 0
+
+with open("input", "r") as file:
+    input = file.read()
+
+ranges: list[str] = input.split(",")
+
+for id_range_str in ranges:
+    id_range: list[str] = id_range_str.split("-")
+
+    for i in range(int(id_range[0]), int(id_range[1])+1):
+        if repeatsPattern(str(i)):
+            sum += i
+
+print(sum)
