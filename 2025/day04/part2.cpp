f0460162cf283139ca537a902a4353044ea93c28
alba4k <blaskoazzolaaaron@gmail.com>
2025-12-04 13:37:05 +0100

    2025: day4

---
+++ 2025/day04/part2.cpp
@@ -0,0 +1,58 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> readToVector(const std::string& filename) {
+    std::vector<std::string> lines;
+    std::ifstream file(filename);
+    if(!file.is_open()) return {};
+
+    std::string line;
+    while (std::getline(file, line))
+        lines.push_back(line);
+
+    return lines;
+}
+
+int clean(std::vector<std::string>& input) {
+    int count = 0;
+
+    for(int i = 0; i < std::size(input); i++) {
+        for(int j = 0; j < input[i].length(); j++) {
+            if(input[i][j] != '@') continue;
+
+            int paper_count = 0;
+            int max_i = std::size(input)-1;
+            int max_j = input[i].length()-1;
+
+            if(i > 0     && j > 0     && input[i-1][j-1] == '@') ++ paper_count; 
+            if(i > 0                  && input[i-1][j]   == '@') ++ paper_count; 
+            if(i > 0     && j < max_j && input[i-1][j+1] == '@') ++ paper_count; 
+            if(             j > 0     && input[i][j-1]   == '@') ++ paper_count; 
+            if(             j < max_j && input[i][j+1]   == '@') ++ paper_count; 
+            if(i < max_i && j > 0     && input[i+1][j-1] == '@') ++ paper_count; 
+            if(i < max_i              && input[i+1][j]   == '@') ++ paper_count; 
+            if(i < max_i && j < max_j && input[i+1][j+1] == '@') ++ paper_count; 
+
+            if(paper_count < 4) {
+                ++count;
+                input[i][j] = '.';
+            }
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    std::vector<std::string> input = readToVector("input");
+    int total = 0;
+    int count = 1;
+    while(count != 0) {
+        count = clean(input);
+        total += count;
+    }
+
+    std::cout << total << std::endl;
+}
